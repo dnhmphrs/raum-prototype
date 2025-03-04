2404be9617d10d2e8e005fed640ec79f167a4500
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2025-03-04 01:57:41 +0000

    preload image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -51,7 +51,7 @@
 	<meta name="author" content="AUFBAU" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<!-- <link rel="preload" href="/aufbau.svg" as="image" type="image/svg+xml" crossorigin="anonymous" /> -->
+	<link rel="preload" href="/notcrowded2.png" as="image" type="image/svg+xml" crossorigin="anonymous" />
 
 	<!-- <link
 		rel="preload"
