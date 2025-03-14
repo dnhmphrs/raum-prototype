3387909cedbbd6fd3aab498b4ca1824114b67e59
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2025-03-14 05:24:50 +0000

    edit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,28 @@ import { defineConfig } from 'vitest/config';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-	plugins: [sveltekit(), glsl()],
+	plugins: [
+		sveltekit(),
+		glsl({
+			include: [
+				'**/*.glsl',
+				'**/*.wgsl',
+			],
+		}),
+		{
+			name: 'vite-plugin-wgsl',
+			transform(code, id) {
+				if (id.endsWith('.wgsl')) {
+					return {
+						code: `export default ${JSON.stringify(code)};`,
+						map: null
+					};
+				}
+			}
+		}
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	assetsInclude: ['**/*.wgsl']
 });
