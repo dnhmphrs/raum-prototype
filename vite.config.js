67e539912e6a296b8a2bee2e178126656f54f16b
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2025-03-14 05:15:00 +0000

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import glsl from 'vite-plugin-glsl';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
 	plugins: [
@@ -21,17 +19,6 @@ export default defineConfig({
 						code: `export default ${JSON.stringify(code)};`,
 						map: null
 					};
-				}
-			}
-		},
-		{
-			name: 'vite-plugin-copy-shaders',
-			writeBundle() {
-				const staticDir = path.resolve('static');
-				const shaderDir = path.join(staticDir, 'shaders');
-				
-				if (fs.existsSync(shaderDir)) {
-					console.log('Copying shader files to build output...');
 				}
 			}
 		}
